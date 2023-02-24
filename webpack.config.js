34d8d4f95d91e84392faaf3a5f45ec1ef73f275c
Vitalii Tereshchenko <metalyx3@gmail.com>
2023-02-24 12:10:07 -0800

    #002: Scss is set up

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,10 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
+const mode = process.env.NODE_ENV === 'development' ? 'development' : 'production';
+
 module.exports = {
-    mode: process.env.NODE_ENV ?? 'production',
+    mode,
     entry: {
         index: path.resolve(__dirname, './src/index.js'),
     },
@@ -12,7 +14,27 @@ module.exports = {
     },
     plugins: [
         new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, './src/index.html')
+            template: path.resolve(__dirname, './src/index.html'),
         })
-    ]
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                  // Creates `style` nodes from JS strings
+                  "style-loader",
+                  // Translates CSS into CommonJS
+                  "css-loader",
+                  // Compiles Sass to CSS
+                  "sass-loader",
+                ],
+            },
+        ]
+    },
+    devServer: {
+        static: path.join(__dirname, './src'),
+        port: 3000,
+        open: true,
+    }
 }
