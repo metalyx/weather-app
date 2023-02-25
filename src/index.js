1a01c61d4903809440d2671087eca6f7d5bfd2d0
Vitalii Tereshchenko <metalyx3@gmail.com>
2023-02-25 08:36:37 -0800

    #004: Prettier is set up

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import './styles/reset.scss';
 import './styles/global.scss';
 import './index.scss';
-import weatherContainerBCImage from'./assets/weather-bg.jpg';
+import weatherContainerBCImage from './assets/weather-bg.jpg';
 
-const weatherContainer = document.getElementsByClassName('weather-container')[0];
+const weatherContainer =
+    document.getElementsByClassName('weather-container')[0];
 
 if (weatherContainer) {
     weatherContainer.style.backgroundImage = `url(${weatherContainerBCImage})`;
